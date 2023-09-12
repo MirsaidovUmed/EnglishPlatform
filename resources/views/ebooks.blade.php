68a32e3cff2b36afe0f0da6507506b122dd836de
Umed <umedmirsaidov61@gmail.com>
2023-09-13 02:11:44 +0500

    adding two blades

--- resources/views/ebooks.blade.php
+++ resources/views/ebooks.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Электронные книги</title>
     <link rel="stylesheet" href="./styles/ebooks/style.css">
-    <link rel="stylesheet" href="./styles/tablet/style-tablet.css">
+    <link rel="stylesheet" href="./styles/ebooks/tablet/style-tablet.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 </head>
 <body>
